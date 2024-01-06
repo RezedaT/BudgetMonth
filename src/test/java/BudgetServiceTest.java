7b289657b0b16271bf6b020c2ff524466cfcba2a
Rezeda Talipova <775SRT@gmail.com>
2024-01-06 23:34:49 +0300

    Added BudgetService.csv

--- src/test/java/BudgetServiceTest.java
+++ src/test/java/BudgetServiceTest.java
@@ -1,16 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.BudgetService.BudgetService;
 
 public class BudgetServiceTest {
-    @Test
-    void shouldCalculateMonth3() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3, 10000, 3000, 20000",
+//            "2, 100000, 60000, 150000"
+//    })
+    @CsvFileSource(files ="src/test/resources/BudgetService.csv")
+    void shouldCalculateMonth3(int expected, int income, int expense, int threshold) {
         BudgetService service = new BudgetService();
-// подготавливаем данные:
-        int income = 10_000;
-        int expense = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+        // подготавливаем данные:
+//        int income = 10_000;
+//        int expense = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
         // вызываем целевой метод:
         int actual = service.calculate(income, expense, threshold);
 
@@ -18,18 +26,18 @@ public class BudgetServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateMonth2() {
-        BudgetService service = new BudgetService();
-// подготавливаем данные:
-        int income = 100_000;
-        int expense = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expense, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    void shouldCalculateMonth2() {
+//        BudgetService service = new BudgetService();
+//        // подготавливаем данные:
+//        int income = 100_000;
+//        int expense = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
+//        // вызываем целевой метод:
+//        int actual = service.calculate(income, expense, threshold);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
